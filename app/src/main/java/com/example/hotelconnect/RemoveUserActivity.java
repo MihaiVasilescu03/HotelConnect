7dd7772e7885b875e313f048c5a61d01d9a109ac
Mihai <MihaiVasilescu03@gmail.com>
2023-10-31 20:56:24 +0200

    Pangina de sergere a angajatilor.

--- app/src/main/java/com/example/hotelconnect/RemoveUserActivity.java
+++ app/src/main/java/com/example/hotelconnect/RemoveUserActivity.java
@@ -8,5 +8,6 @@ public class RemoveUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.remove_user_menu);
+
     }
 }
