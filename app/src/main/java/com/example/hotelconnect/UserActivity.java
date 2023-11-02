1a2f30cbe9af0761ccc2ad0b3fa8285c1f6b4bf4
Mihai <MihaiVasilescu03@gmail.com>
2023-11-02 23:34:52 +0200

    Navbar + fragmente pe activitatea de user!

--- app/src/main/java/com/example/hotelconnect/UserActivity.java
+++ app/src/main/java/com/example/hotelconnect/UserActivity.java
@@ -1,11 +1,34 @@
 package com.example.hotelconnect;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class UserActivity extends LoginActivity {
-    @Override
+import com.example.hotelconnect.databinding.UserMenuBinding;
+
+public class UserActivity extends AppCompatActivity {
+    UserMenuBinding binding;
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.angajat_menu);
+        binding = UserMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new CamereFragment());
+        binding.userBottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId()==R.id.anunturimenu) {
+                replaceFragment(new AnunturiFragment());
+            } else if (item.getItemId()==R.id.cameremenu) {
+                replaceFragment(new CamereFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentmanager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentmanager.beginTransaction();
+        fragmentTransaction.replace(R.id.userFrameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
